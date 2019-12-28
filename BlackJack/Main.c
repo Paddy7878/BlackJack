2f641b3c133bea7125c1f53970406511d847f0e1
Paddy7878 <patrickmurray7878@gmail.com>
2019-12-28 14:56:15 +0000

    Added Shuffle

--- BlackJack/Main.c
+++ BlackJack/Main.c
@@ -1,9 +1,32 @@
 #include<stdio.h>
 #include<conio.h>
+#include<time.h>
+#include<stdlib.h>
+//#include<libgen.h>
+
+
+void shuffle(game);
+
+#define DECKSIZE 52
+#define NCARDS 13
+#define PASSES 8192
+
+typedef struct
+{
+	int t;
+	int np;
+	int nd;
+	int d[];
+}game;
+
 
 void main()
 {
-	int option, numOfPlayers, numOfDecks;
+	int option;
+	int shuf;
+	game *g = malloc(sizeof(g));
+
+	srand(time(NULL));
 
 	printf("============================================\n");
 	printf("=           WELCOME TO BLACKJACK           =\n");
@@ -20,18 +43,72 @@ void main()
 	{
 		do {
 			printf("\nEnter number of players (2-4)? ");
-			scanf("%d", &numOfPlayers);
-		} while (numOfPlayers < 2 || numOfPlayers > 4);
+			scanf("%d", &g->np);
+		} while (g->np < 2 || g->np > 4);
 
 		do {
 			printf("\nPlayer 1, enter number of decks: ");
-			scanf("%d", &numOfDecks);
-		} while (numOfDecks < 1 || numOfDecks > 4);
+			scanf("%d", &g->nd);
+			printf("\n");
+		} while (g->nd < 1 || g->nd > 4);
+
+		for (int i = 0; i < DECKSIZE * g->nd; ++i)
+		{
+			g->d[i] = (i % NCARDS) + 2;
+			printf("%d ", g->d[i]);
+			if (g->d[i] == 14) {
+				printf("\n");
+			}
+		}
+
+		shuffle(g);
+		do {
+			printf("\n\nShuffle Again Yes or No(0 or 1): ");
+			scanf("%d", &shuf);
+
+			if (shuf == 0) {
+				shuffle(g);
+			}
+		} while (shuf != 1);
+
+		printf("\n\nDeck Shuffled");
 	}
-	else if (option == 2) // Load Game
+	else // Load Game
 	{
-
+		
 	}
 
-	getch();
+	_getch();
+}
+
+void shuffle(game *g)
+{
+	int i;
+	int j;
+	int tmp;
+	int n = PASSES;
+
+	printf("\n\n");
+
+
+	while (n--)
+	{
+		i = rand() % (DECKSIZE);
+		j = rand() % (DECKSIZE);
+
+		tmp = g->d[i];
+		g->d[i] = g->d[j];
+		g->d[j] = tmp;
+	}
+
+	j = 1;
+	for (i = 0; i < DECKSIZE; ++i)
+	{
+		++j;
+		printf("%2d ", g->d[i]);
+		if (j % 14 == 0) {
+			printf("\n");
+			j = 1;
+		}
+	}
 }
