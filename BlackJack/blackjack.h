a5ca3e1e1721c9186d5c5760c217d36b764672af
Paddy7878 <patrickmurray7878@gmail.com>
2020-01-05 19:18:23 +0000

    minor edits

---
+++ BlackJack/blackjack.h
@@ -0,0 +1,20 @@
+#pragma once
+
+typedef struct game
+{
+	int t;
+	int np;
+	int nd;
+	int r;
+	int d[206];
+}Game;
+
+typedef struct
+{
+	int h[5];
+	int points;
+	int cardNo;
+	int w;
+	int b;
+	int ace;
+}player;
